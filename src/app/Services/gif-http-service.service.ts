9aaf173baeca3b2589b57f8751061d659068c61c
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-10 07:46:40 +0300

    ///

--- src/app/Services/gif-http-service.service.ts
+++ src/app/Services/gif-http-service.service.ts
@@ -8,32 +8,30 @@ import axios from 'axios';
 })
 export class GifHttpServiceService {
 
-  gif:Gifs;//Service Gif Class
-  gifArray:Gifs[]=[];//Array to Push Gif Urls
+  gif: Gifs;//Service Gif Class
+  gifArray: Gifs[] = [];//Array to Push Gif Urls
   // query:string = "doge";
   constructor() {
     this.gif = new Gifs("");
-   }
+  }
 
 
-   async trendingGif(){
-     try{
-       const response = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.Api_Url}&limit=60&offset=0&rating=g&lang=en`);
-       const responseArray = response.data.data;//Save the response Array to a variable
+  async trendingGif() {
+    try {
+      const response = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.Api_Url}&limit=60&offset=0&rating=g&lang=en`);
+      const responseArray = response.data.data;//Save the response Array to a variable
 
-       /*Loop through each object in the array and get the gif Urls */
-       responseArray.forEach((element:any) => {
+      /*Loop through each object in the array and get the gif Urls */
+      responseArray.forEach((element: any) => {
 
-         if(element.images.original.url){//Handles cases where images are not found
-         this.gifArray.push(new Gifs(element.images.original.url))//Push the gif Urls to the GifArray
-         }
-       });
-       
-       console.log(responseArray);
-       console.log(this.gifArray);
+        this.gifArray.push(new Gifs(element.images.original.url))//Push the gif Urls to the GifArray
+      });
 
-     }catch(error){
-       alert("Nope,no gifs for you");//If no response,alert this
-     }
-   }
+      console.log(responseArray);
+      console.log(this.gifArray);
+
+    } catch (error) {
+      alert("Nope,no gifs for you");//If no response,alert this
+    }
+  }
 }
